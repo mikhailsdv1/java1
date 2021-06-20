9516ee8c11e1a329d55671c860830e0c8a867ad8
msadovskiy <msadovskiy@gogoair.com>
2021-06-20 04:33:25 -0500

    updated

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,6 +1,12 @@
 package com.company;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Solution {
+
+    Logger logger = LoggerFactory.getLogger(Solution.class);
 
     public static void main(String[] args) {
         Solution s = new Solution();
@@ -8,7 +14,7 @@ public class Solution {
     }
 
     public void run(){
-        System.out.println("test");
+        logger.info("test:{}", 123);
     }
 
 
