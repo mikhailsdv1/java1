b457659c41cd76f6e2056a89a0c65823a9a0a4ba
msadovskiy <msadovskiy@gogoair.com>
2021-06-20 04:07:33 -0500

    updated

---
+++ src/com/company/Solution.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.run();
+    }
+
+    public void run(){
+        System.out.println("test");
+    }
+
+
+}
